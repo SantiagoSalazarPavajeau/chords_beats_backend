7e1e4aefc3fe7e726b1845441630a8e388ee153f
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-01 20:28:27 +0200

    launch database, configure cors, and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+synth_wave = Song.create(name: "Synthwave")
+
+am = Chord.create(name: "Harp", file: "assets/chords/Am.wav ", edit_id: 11, song: synth_wave)
+c = Chord.create(name: "Casio", file: "assets/chords/C.wav ", edit_id: 22, song: synth_wave)
+em = Chord.create(name: "Casio", file: "assets/chords/Em.wav ", edit_id: 33, song: synth_wave)
+f = Chord.create(name: "Casio", file: "assets/chords/F.wav ", edit_id: 44, song: synth_wave)
+# order by created at in the controller or in the model
